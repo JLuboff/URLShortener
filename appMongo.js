8b64c33424cbb2b4c9c5374d6d5cb9804fc7e857
Jason Luboff <jluboff@gmail.com>
2017-04-04 22:35:33 +0000

    Callback working

--- appMongo.js
+++ appMongo.js
@@ -2,20 +2,23 @@
 var express = require("express");
 var mongo = require("mongodb").MongoClient;
 var port = process.env.PORT || 8080;
-
+var num = 0;
+var insertUrl;
 var app = express();
 
-function getRandNum(){
-    let num = Math.floor(Math.random() * 9000) + 1000;
+function getRandNum(callback){
+     num = Math.floor(Math.random() * 9000) + 1000;
 
     mongo.connect("mongodb://test:test@ds135800.mlab.com:35800/urlshortener", function(err, db){
         if(err) throw err;
         db.collection("Url").findOne({short_url: num}, {_id: 0, original_url: 1, short_url: 1}, function(err, data){
            if(err) throw err;
-           if(data === null)
-           return num;
-           else 
-           getRandNum();
+           if(data === null){
+           callback(num);
+           }
+           else {
+           getRandNum(callback);
+           }
            db.close();
 });
 });
@@ -23,22 +26,24 @@ function getRandNum(){
 
 //app.get(/\/(.*)/, function(req, res){
 app.get(/\/?(http:\/\/|https:\/\/)(.*)/, function(req, res){
+     
 mongo.connect("mongodb://test:test@ds135800.mlab.com:35800/urlshortener", function(err, db){
     if(err) throw err;
-    console.log("req 0: " + req.params[0], "req 1: " + req.params[1]);
-    let num = getRandNum();
-    console.log(num);
-    let insertUrl = {
+    getRandNum(function(returnNum){
+    console.log("Num inside .get: " + num);
+     insertUrl = {
         original_url: req.params[0] + req.params[1],
         short_url: num
     };
-console.log(insertUrl);
+    console.log(insertUrl);
+
     db.collection("Url").insert(insertUrl, function(err, data){
         if(err) throw err;
         console.log("Saved to DB");
         res.send({"original_url": insertUrl.original_url, "short_url" : insertUrl.short_url });
         db.close();
     });
+});
 });
 });
 

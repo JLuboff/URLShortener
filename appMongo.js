8059dced90e9a8e91174e269e006d4a9500adf2d
Jason Luboff <jluboff@gmail.com>
2017-04-04 23:59:28 +0000

    Code formatted. Need index file

--- appMongo.js
+++ appMongo.js
@@ -6,59 +6,58 @@ var num = 0;
 var app = express();
 
 function getRandNum(callback){
-     num = Math.floor(Math.random() * 9000) + 1000;
+  num = Math.floor(Math.random() * 9000) + 1000;
 
-    mongo.connect("mongodb://test:test@ds135800.mlab.com:35800/urlshortener", function(err, db){
-        if(err) throw err;
-        db.collection("Url").findOne({short_url: num}, {_id: 0, original_url: 1, short_url: 1}, function(err, data){
-           if(err) throw err;
-           //Checks if any data is find with the current number, if not we send back num inside our callback which goes inside our app.get
-           if(data === null){
-           callback(num);
-           }
-           //Else, a document is found, we rerun the function to create a new random number
-           else {
-           getRandNum(callback);
-           }
-           db.close();
-});
-});
+  mongo.connect("mongodb://test:test@ds135800.mlab.com:35800/urlshortener", function(err, db){
+    if(err) throw err;
+    db.collection("Url").findOne({short_url: num}, {_id: 0, original_url: 1, short_url: 1}, function(err, data){
+      if(err) throw err;
+      //Checks if any data is find with the current number, if not we send back num inside our callback which goes inside our app.get
+      if(data === null){
+        callback(num);
+      }
+      //Else, a document is found, we rerun the function to create a new random number
+      else {
+        getRandNum(callback);
+      }
+      db.close();
+    });
+  });
 }
 
 app.get(/\/?(http:\/\/|https:\/\/)(.*)/, function(req, res){
-     
-mongo.connect("mongodb://test:test@ds135800.mlab.com:35800/urlshortener", function(err, db){
+
+  mongo.connect("mongodb://test:test@ds135800.mlab.com:35800/urlshortener", function(err, db){
     if(err) throw err;
     //We make a call to our getRandNum function which will return our random number after it has been checked
     getRandNum(function(returnNum){
-    let insertUrl = {
+      let insertUrl = {
         original_url: req.params[0] + req.params[1],
         short_url: num
-    };
-    db.collection("Url").insert(insertUrl, function(err, data){
+      };
+      db.collection("Url").insert(insertUrl, function(err, data){
         if(err) throw err;
-        console.log("Saved to DB");
-        res.send({"original_url": insertUrl.original_url, "short_url" : insertUrl.short_url });
+        res.send({"original_url": insertUrl.original_url, "short_url" : req.protocol + "://" + req.get("host") + "/" + insertUrl.short_url });
         db.close();
+      });
     });
-});
-});
+  });
 });
 
 app.get(/\/\b\d{4}\b/, function(req, res){
-    mongo.connect("mongodb://test:test@ds135800.mlab.com:35800/urlshortener", function(err, db){
-        if(err) throw err;
-        let num = Number(req.url.slice(1));
-        console.log(num);
-        db.collection("Url").findOne({short_url: num}, {_id: 0, original_url: 1, short_url: 1}, function(err, data){
-           if(err) throw err;
-       if(data === null)
-       res.send("Invalid input.");
-       else
-    res.redirect(data.original_url);
-        db.close();
-        });     
-});
+  mongo.connect("mongodb://test:test@ds135800.mlab.com:35800/urlshortener", function(err, db){
+    if(err) throw err;
+    let num = Number(req.url.slice(1));
+    console.log(num);
+    db.collection("Url").findOne({short_url: num}, {_id: 0, original_url: 1, short_url: 1}, function(err, data){
+      if(err) throw err;
+      if(data === null)
+      res.send("Invalid input.");
+      else
+      res.redirect(data.original_url);
+      db.close();
+    });
+  });
 });
 
 app.listen(port);
